2320383c91057f4b830fd849984c5a519ab3bf59
rizwanishaq <rizwanishaq@gmail.com>
2022-12-29 12:20:51 +0100

    first commit

---
+++ server/routes/openaiRoutes.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const { generateImage } = require("../controllers/openaiController");
+router.post("/generateimage", generateImage);
+
+module.exports = router;
